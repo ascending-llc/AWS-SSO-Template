263d804e13d0b0077abf2049f5ba8f56f3cdd752
Qiaodan Xie <qiaodanxie@Qiaodans-MacBook-Pro.local>
2022-12-20 14:28:24 -0500

    merge to serverless sam deployment

--- lambda/lambda-provision.py
+++ lambda/lambda-provision.py
@@ -6,17 +6,21 @@ import requests
 
 
 def lambda_handler(event, context):
-    
-    ssm_client = boto3.client('ssm')
+    SecretArn = os.environ.get("SecretArn")
+    scope = os.environ.get("GraphScope")
+    authority= os.environ.get("GraphAuthority")
+    beta_url = os.environ.get("GraphBetaUrl")
+    graph_url = os.environ.get("GraphUrl")
 
-    client_id = ssm_client.get_parameter(Name='clientId')['Parameter']['Value']
-    client_secret = ssm_client.get_parameter(Name='clientSecret')['Parameter']['Value']
-    scope = 'https://graph.microsoft.com/.default'
-    authority='https://login.microsoftonline.com/{}'
-    tenant_id = ssm_client.get_parameter(Name='tenantId')['Parameter']['Value']
+    secrets_client = boto3.client("secretsmanager")
+    response = secrets_client.get_secret_value(SecretId=SecretArn)
+    secrets = json.loads(response['SecretString'])
 
-    secret_url = ssm_client.get_parameter(Name='SCIMEndpoint')['Parameter']['Value']
-    secret_token = ssm_client.get_parameter(Name='AccessToken')['Parameter']['Value']
+    client_id = secrets['clientId']
+    client_secret = secrets['clientSecret']
+    tenant_id = secrets['tenantId']
+    secret_url = secrets['SCIMEndpoint']
+    secret_token = secrets['AccessToken']
 
     client = ConfidentialClientApplication(client_id=client_id,client_credential=client_secret,authority=authority.format(tenant_id))
     ret_dic = client.acquire_token_for_client(scope)
@@ -30,23 +34,23 @@ def lambda_handler(event, context):
     }
     
     url_dic={
-        'provision_template': 'https://graph.microsoft.com/beta/servicePrincipals/{}/synchronization/templates',
-        'create_job': 'https://graph.microsoft.com/beta/servicePrincipals/{}/synchronization/jobs',
-        'connection': 'https://graph.microsoft.com/beta/servicePrincipals/{}/synchronization/jobs/{}/validateCredentials',
-        'save_secret': 'https://graph.microsoft.com/beta/servicePrincipals/{}/synchronization/secrets',
-        'start_provision': 'https://graph.microsoft.com/beta/servicePrincipals/{}/synchronization/jobs/{}/start',
-        'get_sp': "https://graph.microsoft.com/v1.0/servicePrincipals?$filter=displayName eq 'AWS Single Sign-on'"
+        'provision_template': '{}{}/synchronization/templates',
+        'create_job': '{}{}/synchronization/jobs',
+        'connection': '{}{}/synchronization/jobs/{}/validateCredentials',
+        'save_secret': '{}{}/synchronization/secrets',
+        'start_provision': '{}{}/synchronization/jobs/{}/start',
+        'get_sp': "{}servicePrincipals?$filter=displayName eq 'AWS Single Sign-on'"
     }
 
 
     
-    app_id = get_sp_id(url_dic,header_dic)
-    job_template_id = provision_job(url_dic,header_dic,app_id)
-    job_id = create_provision_job(url_dic,header_dic,app_id,job_template_id)
-    connection_code = make_connection(url_dic,header_dic,app_id,job_id,secret_url,secret_token)
+    app_id = get_sp_id(url_dic,header_dic,graph_url)
+    job_template_id = provision_job(url_dic,header_dic,app_id,beta_url)
+    job_id = create_provision_job(url_dic,header_dic,app_id,job_template_id,beta_url)
+    connection_code = make_connection(url_dic,header_dic,app_id,job_id,secret_url,secret_token,beta_url)
     if connection_code == 204:
-        save_connection(url_dic,header_dic,app_id,secret_url,secret_token)
-        r = requests.post(url_dic['start_provision'].format(app_id,job_id), headers=header_dic['header_2'])
+        save_connection(url_dic,header_dic,app_id,secret_url,secret_token,beta_url)
+        r = requests.post(url_dic['start_provision'].format(beta_url,app_id,job_id), headers=header_dic['header_2'])
     
         return {
             'statusCode': 200,
@@ -55,29 +59,29 @@ def lambda_handler(event, context):
 
 
 
-def get_sp_id(url_dic,header_dic):
-    r = requests.get(url_dic['get_sp'], headers=header_dic['header_2'])
+def get_sp_id(url_dic,header_dic,graph_url):
+    r = requests.get(url_dic['get_sp'].format(graph_url), headers=header_dic['header_2'])
     app_id = r.json()['value'][0]['id']
     return app_id
 
 
-def provision_job(url_dic,header_dic,app_id):
-    r = requests.get(url_dic['provision_template'].format(app_id), headers=header_dic['header_2'])
+def provision_job(url_dic,header_dic,app_id,beta_url):
+    r = requests.get(url_dic['provision_template'].format(beta_url,app_id), headers=header_dic['header_2'])
     job_template_id = r.json()['value'][0]['id']
     return job_template_id
 
 
-def create_provision_job(url_dic,header_dic,app_id,job_template_id):
+def create_provision_job(url_dic,header_dic,app_id,job_template_id,beta_url):
     data = {
         "templateId": job_template_id
     }
-    r = requests.post(url_dic['create_job'].format(app_id), headers=header_dic['header_2'],json=data)
+    r = requests.post(url_dic['create_job'].format(beta_url,app_id), headers=header_dic['header_2'],json=data)
     job_id = r.json()['id']
 
     return job_id
 
 
-def make_connection(url_dic,header_dic,app_id,job_id,secret_url,secret_token):
+def make_connection(url_dic,header_dic,app_id,job_id,secret_url,secret_token,beta_url):
     data ={
         "credentials": [ 
             { 
@@ -88,12 +92,12 @@ def make_connection(url_dic,header_dic,app_id,job_id,secret_url,secret_token):
             }
         ]
     }
-    r = requests.post(url_dic['connection'].format(app_id,job_id), headers=header_dic['header_2'],json=data)
+    r = requests.post(url_dic['connection'].format(beta_url,app_id,job_id), headers=header_dic['header_2'],json=data)
     
     return r.status_code
 
 
-def save_connection(url_dic,header_dic,app_id,secret_url,secret_token):
+def save_connection(url_dic,header_dic,app_id,secret_url,secret_token,beta_url):
     data ={
         "value": [ 
             { 
@@ -104,7 +108,7 @@ def save_connection(url_dic,header_dic,app_id,secret_url,secret_token):
             }
         ]
     }
-    r = requests.put(url_dic['save_secret'].format(app_id), headers=header_dic['header_2'],json=data)
+    r = requests.put(url_dic['save_secret'].format(beta_url,app_id), headers=header_dic['header_2'],json=data)
     return r.status_code
 
 
--- lambda/lambda-sso.py
+++ lambda/lambda-sso.py
@@ -3,21 +3,26 @@ import os, re, base64
 import boto3
 from msal import ConfidentialClientApplication
 import requests
-
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 
 def lambda_handler(event, context):
-    
-    ssm_client = boto3.client('ssm')
-    # environment variable for graph_api https://graph.microsoft.com/
-    client_id = ssm_client.get_parameter(Name='clientId')['Parameter']['Value']
-    client_secret = ssm_client.get_parameter(Name='clientSecret')['Parameter']['Value']
-    scope = 'https://graph.microsoft.com/.default'
-    authority='https://login.microsoftonline.com/{}'
-    tenant_id = ssm_client.get_parameter(Name='tenantId')['Parameter']['Value']
-    
-    identifier = ssm_client.get_parameter(Name='identityUrl')['Parameter']['Value']
-    redirect = ssm_client.get_parameter(Name='acsUrl')['Parameter']['Value']
-    
+    SecretArn = os.environ.get("SecretArn")
+    GraphUrl = os.environ.get("GraphUrl")
+    appName = os.environ.get("AWSAppName")
+    scope = os.environ.get("GraphScope")
+    authority= os.environ.get("GraphAuthority")
+    group_names = [name for name in os.environ.get("GroupNames").split(",") if name]
+
+    secrets_client = boto3.client("secretsmanager")
+    response = secrets_client.get_secret_value(SecretId=SecretArn)
+    secrets = json.loads(response['SecretString'])
+
+    client_id = secrets['clientId']
+    client_secret = secrets['clientSecret']
+    tenant_id = secrets['tenantId']
+    identifier = secrets['identityUrl']
+    redirect = secrets['acsUrl']
     
     client = ConfidentialClientApplication(client_id=client_id,client_credential=client_secret,authority=authority.format(tenant_id))
     ret_dic = client.acquire_token_for_client(scope)
@@ -31,25 +36,25 @@ def lambda_handler(event, context):
     }
     
     url_dic={
-        'create_group': 'https://graph.microsoft.com/v1.0/groups',
-        'get_template_url': "https://graph.microsoft.com/v1.0/applicationTemplates?$filter=displayName eq 'AWS IAM Identity Center (successor to AWS Single Sign-On)'",
-        'create_app_url': "https://graph.microsoft.com/v1.0/applicationTemplates/{}/instantiate",
-        'configure_sso_url': "https://graph.microsoft.com/v1.0/servicePrincipals/{}",
-        'set_saml_url': "https://graph.microsoft.com/v1.0/applications/{}",
-        'certificate_url': 'https://graph.microsoft.com/v1.0/servicePrincipals/{}/addTokenSigningCertificate',
-        'add_group': 'https://graph.microsoft.com/v1.0/servicePrincipals/{}/appRoleAssignedTo'
+        'create_group': '{}groups',
+        'get_template_url': "{}applicationTemplates?$filter=displayName eq '{}'",
+        'create_app_url': "{}applicationTemplates/{}/instantiate",
+        'configure_sso_url': "{}servicePrincipals/{}",
+        'set_saml_url': "{}applications/{}",
+        'certificate_url': '{}servicePrincipals/{}/addTokenSigningCertificate',
+        'add_group': '{}servicePrincipals/{}/appRoleAssignedTo'
     }
     
 
     
-    application_ids = create_aws_app(url_dic,header_dic)
+    application_ids = create_aws_app(url_dic,header_dic,GraphUrl,appName)
     
     
     if application_ids[2] == 201:
-        configure_saml(url_dic,header_dic,application_ids,identifier,redirect)
-        certificate(url_dic,header_dic,application_ids)
-        group_ids = create_groups(url_dic,header_dic)
-        add_groups(url_dic,header_dic,group_ids,application_ids)
+        configure_saml(url_dic,header_dic,application_ids,identifier,redirect,GraphUrl)
+        certificate(url_dic,header_dic,application_ids,GraphUrl)
+        group_ids = create_groups(url_dic,header_dic,GraphUrl,group_names)
+        add_groups(url_dic,header_dic,group_ids,application_ids,GraphUrl)
         
         
         return {
@@ -57,21 +62,20 @@ def lambda_handler(event, context):
             "body": "success"
         } 
     
-    
-def add_groups(url_dic,header_dic,group_ids,ids):
+def add_groups(url_dic,header_dic,group_ids,ids,graph_url):
     for g_id in group_ids:
         data ={
             'principalId': g_id, ##group id
             'resourceId': ids[0], ## app object id
             'appRoleId': ids[3] ## app role id
         }
-        r = requests.post(url_dic['add_group'].format(ids[0]), headers=header_dic['header_1'],json=data)
+        r = requests.post(url_dic['add_group'].format(graph_url,ids[0]), headers=header_dic['header_1'],json=data)
         #print(r.status_code)
 
 
-# environment variable 
-def create_groups(url_dic,header_dic):
-    group_name = ['admin-group', 'power-user-group', 'read-only-group']
+
+def create_groups(url_dic,header_dic,graph_url,group_names):
+    group_name = group_names
     group_ids = []
     for name in group_name:
         group_data ={
@@ -81,22 +85,21 @@ def create_groups(url_dic,header_dic):
         	'securityEnabled': True,
         	'groupTypes': []
         }
-        r = requests.post(url_dic['create_group'], headers=header_dic['header_1'],json=group_data)
+        r = requests.post(url_dic['create_group'].format(graph_url), headers=header_dic['header_1'],json=group_data)
         result = r.json()
         group_ids.append(result['id'])
     return group_ids
 
-# functional varabile AWS Single Sign-on
-def create_aws_app(url_dic,header_dic):
-    
-    r = requests.get(url_dic['get_template_url'], headers=header_dic['header_1'])
+
+def create_aws_app(url_dic,header_dic,graph_url,appName):
+    r = requests.get(url_dic['get_template_url'].format(graph_url,appName), headers=header_dic['header_1'])
     result = r.json()
     template_id = result['value'][0]['id']
     
     body ={
 	    'displayName': 'AWS Single Sign-on'
     }
-    r1 = requests.post(url_dic['create_app_url'].format(template_id),headers=header_dic['header_1'],json=body)
+    r1 = requests.post(url_dic['create_app_url'].format(graph_url,template_id),headers=header_dic['header_1'],json=body)
     result1 = r1.json()
     sp_id = result1['servicePrincipal']['id']
     app_id = result1['application']['id']
@@ -105,15 +108,21 @@ def create_aws_app(url_dic,header_dic):
     return[sp_id,app_id,r1.status_code,role_id]
 
 
-def configure_saml(url_dic,header_dic,ids,identifier,redirect):
+def configure_saml(url_dic,header_dic,ids,identifier,redirect,graph_url):
     body2 ={
 	    'preferredSingleSignOnMode': 'saml'
     }
 
-    check_code = 0
-    while(check_code != 204):
-        r2 = requests.patch(url_dic['configure_sso_url'].format(ids[0]),headers=header_dic['header_1'],json=body2)
-        check_code = r2.status_code
+    requests_session = requests.Session() #
+    requests_session2 = requests.Session()
+    retries = Retry( 
+        total=4, 
+        backoff_factor=3, 
+        status_forcelist=[403, 404],
+        allowed_methods= ["PATCH"]
+    )
+    requests_session.mount("https://", HTTPAdapter(max_retries=retries)) 
+    r2 = requests_session.patch(url_dic['configure_sso_url'].format(graph_url,ids[0]),headers=header_dic['header_1'],json=body2)
 
     body3 = {
     	"web": {
@@ -125,23 +134,21 @@ def configure_saml(url_dic,header_dic,ids,identifier,redirect):
         	identifier
       	]
     }
-    check_code = 0
-    while(check_code != 204):
-        r3 = requests.patch(url_dic['set_saml_url'].format(ids[1]),headers=header_dic['header_1'],json=body3)
-        check_code = r3.status_code
+    requests_session2.mount("https://", HTTPAdapter(max_retries=retries)) 
+    r3 = requests_session2.patch(url_dic['set_saml_url'].format(graph_url,ids[1]),headers=header_dic['header_1'],json=body3)
 
 
-def certificate(url_dic,header_dic,ids):
+def certificate(url_dic,header_dic,ids,graph_url):
     body4 ={
         "displayName":"CN=AWSSingleSignOn",
         "endDateTime": None
     }
-    r4 = requests.post(url_dic['certificate_url'].format(ids[0]),headers=header_dic['header_1'],json=body4)
+    r4 = requests.post(url_dic['certificate_url'].format(graph_url,ids[0]),headers=header_dic['header_1'],json=body4)
 
     thumbprint = r4.json()['thumbprint']
 
     body5={
         "preferredTokenSigningKeyThumbprint": thumbprint
     }
-    r5 = requests.patch(url_dic['configure_sso_url'].format(ids[0]),headers=header_dic['header_1'],json=body5)
+    r5 = requests.patch(url_dic['configure_sso_url'].format(graph_url,ids[0]),headers=header_dic['header_1'],json=body5)
 
